c66f4699248b4311b03d615be8dc4f0ffe80c055
Maria Schuld <mariaschuld@gmail.com>
2020-10-20 19:35:27 +0200

    comment out bert

--- mma_word_embeddings/trainable_model.py
+++ mma_word_embeddings/trainable_model.py
@@ -4,8 +4,8 @@ import numpy as np
 from gensim.models import Word2Vec
 from gensim.models.keyedvectors import WordEmbeddingsKeyedVectors
 
-import torch
-from transformers import BertTokenizer, BertModel
+# import torch
+# from transformers import BertTokenizer, BertModel
 
 
 class TrainableModel:
@@ -112,85 +112,85 @@ class Word2VecModel(TrainableModel):
 
         return emb
 
-
-class BertModel(TrainableModel):
-    """Train a word embedding using a BERT model."""
-
-    def __init__(self, path_training_data, path_description):
-        super().__init__(path_training_data, path_description)
-        self.MAX_LEN = 50
-
-    def _prepare_data(self):
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
-        #max_document_length = np.max([len(document.split(' ')) for document in self.training_data])
-
-        input_ids = []
-        tokenized_texts = []
-
-        for doc in self.training_data:
-            encoded_dict = tokenizer.encode_plus(
-                doc,  # Sentence to encode.
-                add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
-                truncation=True,
-                max_length=self.MAX_LEN,  # Pad & truncate all sentences.
-                pad_to_max_length=True,
-                return_tensors='pt',  # Return pytorch tensors.
-            )
-
-            # Save tokens from sentence as a separate array. We will use it later to explore and compare embeddings.
-            marked_text = "[CLS] " + doc + " [SEP]"
-            tokenized_texts.append(marked_text.split(' '))
-
-            # Add the encoded sentence to the list.
-            input_ids.append(encoded_dict['input_ids'])
-
-        # Convert the list into tensor.
-        input_ids = torch.cat(input_ids, dim=0)
-
-        return input_ids, tokenized_texts
-
-    def make_embedding(self, hyperparameters):
-        """Train a BERT embedding."""
-
-        input_ids, tokenized_texts = self._prepare_data()
-        segments_ids = torch.ones_like(input_ids)
-        NUM_DOCS = len(self.training_data)
-
-        model = BertModel.from_pretrained('bert-base-uncased',
-                                          output_hidden_states=True,  # Whether the model returns all hidden-states.
-                                          )
-        model.eval()
-
-        with torch.no_grad():
-            outputs = model(input_ids, segments_ids)
-
-            # Evaluating the model will return a different number of objects based on
-            # how it's  configured in the `from_pretrained` call earlier. In this case,
-            # becase we set `output_hidden_states = True`, the third item will be the
-            # hidden states from all layers. See the documentation for more details:
-            # https://huggingface.co/transformers/model_doc/bert.html#bertmodel
-            hidden_states = outputs[2]
-
-        token_embeddings = torch.stack(hidden_states, dim=0)
-        token_embeddings = token_embeddings.permute(1, 2, 0, 3)
-        processed_embeddings = token_embeddings[:, :, 9:, :]
-        embeddings = torch.reshape(processed_embeddings, (NUM_DOCS, self.MAX_LEN, -1))
-
-        # extract dictionary of words and vecs
-        bert_dict = {}
-        # for i in tokenized_texts:
-        for i in range(len(tokenized_texts)):
-            for j in range(len(embeddings)):
-                if i == j:
-                    for k in range(len(tokenized_texts[i])):
-                        for l in range(len(embeddings[j])):
-                            if k == l:
-                                bert_dict['{}'.format(tokenized_texts[i][k])] = embeddings[j][l].numpy()
-
-        # turn dict to gensim embedding
-        emb = WordEmbeddingsKeyedVectors(vector_size=len(list(bert_dict.values())[0]))
-        entities = list(bert_dict.keys())
-        weights = list(bert_dict.values())
-        emb.add(entities, weights)
-
-        return emb
+#
+# class BertModel(TrainableModel):
+#     """Train a word embedding using a BERT model."""
+#
+#     def __init__(self, path_training_data, path_description):
+#         super().__init__(path_training_data, path_description)
+#         self.MAX_LEN = 50
+#
+#     def _prepare_data(self):
+#         tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+#         #max_document_length = np.max([len(document.split(' ')) for document in self.training_data])
+#
+#         input_ids = []
+#         tokenized_texts = []
+#
+#         for doc in self.training_data:
+#             encoded_dict = tokenizer.encode_plus(
+#                 doc,  # Sentence to encode.
+#                 add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
+#                 truncation=True,
+#                 max_length=self.MAX_LEN,  # Pad & truncate all sentences.
+#                 pad_to_max_length=True,
+#                 return_tensors='pt',  # Return pytorch tensors.
+#             )
+#
+#             # Save tokens from sentence as a separate array. We will use it later to explore and compare embeddings.
+#             marked_text = "[CLS] " + doc + " [SEP]"
+#             tokenized_texts.append(marked_text.split(' '))
+#
+#             # Add the encoded sentence to the list.
+#             input_ids.append(encoded_dict['input_ids'])
+#
+#         # Convert the list into tensor.
+#         input_ids = torch.cat(input_ids, dim=0)
+#
+#         return input_ids, tokenized_texts
+#
+#     def make_embedding(self, hyperparameters):
+#         """Train a BERT embedding."""
+#
+#         input_ids, tokenized_texts = self._prepare_data()
+#         segments_ids = torch.ones_like(input_ids)
+#         NUM_DOCS = len(self.training_data)
+#
+#         model = BertModel.from_pretrained('bert-base-uncased',
+#                                           output_hidden_states=True,  # Whether the model returns all hidden-states.
+#                                           )
+#         model.eval()
+#
+#         with torch.no_grad():
+#             outputs = model(input_ids, segments_ids)
+#
+#             # Evaluating the model will return a different number of objects based on
+#             # how it's  configured in the `from_pretrained` call earlier. In this case,
+#             # becase we set `output_hidden_states = True`, the third item will be the
+#             # hidden states from all layers. See the documentation for more details:
+#             # https://huggingface.co/transformers/model_doc/bert.html#bertmodel
+#             hidden_states = outputs[2]
+#
+#         token_embeddings = torch.stack(hidden_states, dim=0)
+#         token_embeddings = token_embeddings.permute(1, 2, 0, 3)
+#         processed_embeddings = token_embeddings[:, :, 9:, :]
+#         embeddings = torch.reshape(processed_embeddings, (NUM_DOCS, self.MAX_LEN, -1))
+#
+#         # extract dictionary of words and vecs
+#         bert_dict = {}
+#         # for i in tokenized_texts:
+#         for i in range(len(tokenized_texts)):
+#             for j in range(len(embeddings)):
+#                 if i == j:
+#                     for k in range(len(tokenized_texts[i])):
+#                         for l in range(len(embeddings[j])):
+#                             if k == l:
+#                                 bert_dict['{}'.format(tokenized_texts[i][k])] = embeddings[j][l].numpy()
+#
+#         # turn dict to gensim embedding
+#         emb = WordEmbeddingsKeyedVectors(vector_size=len(list(bert_dict.values())[0]))
+#         entities = list(bert_dict.keys())
+#         weights = list(bert_dict.values())
+#         emb.add(entities, weights)
+#
+#         return emb
